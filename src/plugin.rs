1bde798cf569667937be3c7f14007a691a28b571
Jacobtread <jacobtread@gmail.com>
2025-04-23 20:21:35 +1200

    feat: add other os and arch types

--- src/plugin.rs
+++ src/plugin.rs
@@ -279,6 +279,8 @@ fn is_valid_action_name(value: &str, _context: &()) -> garde::Result {
 pub enum OperatingSystem {
     #[strum(serialize = "windows")]
     Windows,
+    #[strum(serialize = "macos")]
+    MacOs,
     #[strum(serialize = "linux")]
     Linux,
 }
@@ -294,6 +296,11 @@ pub fn platform_os() -> OperatingSystem {
     OperatingSystem::Windows
 }
 
+#[cfg(target_os = "macos")]
+pub fn platform_os() -> OperatingSystem {
+    OperatingSystem::MacOs
+}
+
 #[cfg(target_os = "linux")]
 pub fn platform_os() -> OperatingSystem {
     OperatingSystem::Linux
@@ -307,6 +314,10 @@ pub enum Arch {
     X86,
     #[strum(serialize = "x64")]
     X64,
+    #[strum(serialize = "arm")]
+    Arm,
+    #[strum(serialize = "arm64")]
+    Arm64,
 }
 
 impl Default for Arch {
@@ -329,4 +340,20 @@ pub fn platform_arch() -> Arch {
 ))]
 pub fn platform_arch() -> Arch {
     Arch::X86
+}
+
+#[cfg(all(
+    target_pointer_width = "64",
+    any(target_arch = "arm", target_arch = "aarch64")
+))]
+pub fn platform_arch() -> Arch {
+    Arch::Arm64
+}
+
+#[cfg(all(
+    target_pointer_width = "32",
+    any(target_arch = "arm", target_arch = "aarch64")
+))]
+pub fn platform_arch() -> Arch {
+    Arch::Arm
 }
